3733a8bd8de587e5ffcb886994fb844445cfb82d
Daniel Kendell <daniel@kendell.org>
2014-11-08 22:14:20 +0000

    try this

--- lib/mclight.rb
+++ lib/mclight.rb
@@ -6,27 +6,44 @@ module Mclight
 
   class Runner
     def execute!
-      Light.new( 'minecraft.kendell.org.uk', 1 ).update
+      Minecraft.new( 'minecraft.kendell.org.uk', Light.new( 1 ) ).update
+      Light.new( 4 ).on
     end
   end
 
   class Light
 
-    def initialize( server, pin )
-      @server = server
+    def initialize( pin )
       @pin = pin
 
       @io = WiringPi::GPIO.new
       @io.mode(@pin, OUTPUT)
     end
 
+    def on
+      @io.write( @pin, 1 )
+    end
+
+    def of
+      @io.write( @pin, 0 )
+    end
+
+  end
+
+  class Minecraft
+
+    def initialize( server, light )
+      @server = server
+      @light = light
+    end
+
     def update
       q = Query::simpleQuery( @server, 25565 )
 
       if q[:numplayers] == "0"
-        @io.write( @pin, 0 )
+        @light.off
       else
-        @io.write( @pin, 1 )
+        @light.on
       end
     end
 
